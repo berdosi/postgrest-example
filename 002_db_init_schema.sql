22ae71dbcafbe1ff9350fd7a6651e3fb41e240b2
Balint Erdosi <erdosib@gmail.com>
2020-12-21 02:06:54 +0100

    cleanup duplicate code
    Signed-off-by: Balint Erdosi <erdosib@gmail.com>

--- 002_db_init_schema.sql
+++ 002_db_init_schema.sql
@@ -1,14 +1,7 @@
---      https://postgrest.org/en/v7.0.0/schema_structure.html :
---      By default, when a function is created, the privilege to execute it is 
---      not restricted by role. The function access is PUBLIC—executable by 
---      all roles (more details at PostgreSQL Privileges page). 
---      This is not ideal for an API schema. To disable this behavior, you can 
---      run the following SQL statement:
-ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON FUNCTIONS FROM PUBLIC;
-
--- Create schemas
+-- Create master_data schema.
 
 -- master_data schema contains the raw data : list of SKU, their stock / price changes
+-- This is not exposed to the API.
 
 CREATE SCHEMA "master_data";
 
@@ -107,5 +100,4 @@ CREATE VIEW "master_data"."stock" AS
 -----------------------------------------------------------------
 CREATE TABLE "master_data"."configuration" (
     "db_version" INTEGER,
-    "salt" VARCHAR
 );
