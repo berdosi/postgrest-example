d4b22c9beeca2f9e44c798e48a7b283185fcef38
Balint Erdosi <erdosib@gmail.com>
2020-12-21 02:06:29 +0100

    stub auth
    Signed-off-by: Balint Erdosi <erdosib@gmail.com>

---
+++ 003_auth.sql
@@ -0,0 +1,14 @@
+--  TODO
+
+--  validate login:
+--      check username, password; return JWT
+
+--  create user
+--      add email to master_data.user
+--      set confirmation link
+--      send confirmation link
+
+--  confirm user
+--      email, password, confirmation:
+--      if the confirmation matches the one for the e-mail, and is recent,
+--      set the password, and remove the confirmation
